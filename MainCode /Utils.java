6d8c70649e3fa08d140461ca732d807bf6dd1ae5
Adakhan <147408734+Adahan73@users.noreply.github.com>
2025-05-19 05:34:31 +0600

    Create Utils.java

---
+++ MainCode /Utils.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Utils {
+    public static final Scanner scanner = new Scanner(System.in);
+
+    public static String readNonEmptyString(String prompt) {
+        String input;
+        do {
+            System.out.print(prompt);
+            input = scanner.nextLine().trim();
+            if (input.isEmpty()) System.out.println("Input cannot be empty!");
+        } while (input.isEmpty());
+        return input;
+    }
+
+    public static int readInt(String prompt) {
+        while (true) {
+            try {
+                System.out.print(prompt);
+                return Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid number!");
+            }
+        }
+    }
+
+    public static double readDouble(String prompt) {
+        while (true) {
+            try {
+                System.out.print(prompt);
+                return Double.parseDouble(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid number!");
+            }
+        }
+    }
+}
